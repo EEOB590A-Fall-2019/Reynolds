147721ec81adcc1dabbf7b9824da42b8aed41ae6
Rebekah Reynolds <rebekahr@iastate.edu>
2019-12-10 14:33:13 -0600

    hi

---
+++ data wrangling of aedes 2018.R
@@ -0,0 +1,14 @@
+Aedesdata<-read.csv("/Users/rebekah/Desktop/EEOB 590/Reynolds/data/2018 Aedes (1).csv")
+getwd()
+Aedesdata<-read.csv(data/2018 Aedes (1).csv)
+setwd("Users/rebekah/Desktop/EEOB 590/Reynolds/data")
+getwd()
+read.csv("data/ 2018 Aedes (1).csv")
+read.csv(data/ 2018 Aedes (1).csv)
+read.csv("data /2018 Aedes (1).csv")
+aedesdata<-read.csv("data /2018 Aedes (1).csv")
+aedesdata
+tolower(Year)
+?tolower
+install.packages("tidyverse")
+library(tidyverse)
